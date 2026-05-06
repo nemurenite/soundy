4e7592b5cacafa62b35fe55479ae6c8515c90d7e
nemurenite <nemurenite@gmail.com>
2026-05-06 18:18:16 +0900

    Update key_script.js

--- key_script.js
+++ key_script.js
@@ -1,17 +1,36 @@
-// JS
-const textarea = document.querySelector('.textarea');
+let textarea = document.getElementById("test-target"),
+  consoleLog = document.getElementById("console-log"),
+  btnReset = document.getElementById("btn-reset");
 
-// キーが押されたとき
-textarea.addEventListener('keydown', () => {
-    console.log('キーが押されました。');
+function logMessage(message) {
+  consoleLog.innerHTML += `${message}<br>`;
+}
+
+textarea.addEventListener("keydown", (e) => {
+  if (!e.repeat) {
+    logMessage(`Key "${e.key}" pressed [event: keydown]`);
+  } else {
+    logMessage(`Key "${e.key}" repeating [event: keydown]`);
+  }
 });
 
-// 文字が入力された
-textarea.addEventListener('keypress', () => {
-    console.log('文字が入力されました。');
+textarea.addEventListener("beforeinput", (e) => {
+  logMessage(`Key "${e.data}" about to be input [event: beforeinput]`);
 });
 
-// キーが離されたとき
-textarea.addEventListener('keyup', () => {
-    console.log('キーが離されました。');
+textarea.addEventListener("input", (e) => {
+  logMessage(`Key "${e.data}" input [event: input]`);
+});
+
+textarea.addEventListener("keyup", (e) => {
+  logMessage(`Key "${e.key}" released [event: keyup]`);
+});
+
+btnReset.addEventListener("click", (e) => {
+  let child = consoleLog.firstChild;
+  while (child) {
+    consoleLog.removeChild(child);
+    child = consoleLog.firstChild;
+  }
+  textarea.value = "";
 });
